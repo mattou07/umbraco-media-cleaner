881d2e865f137d520f12e920af0fe28dca8d5018
Matthew Hart <matt@moriyama.co.uk>
2018-10-13 12:16:27 +0100

    Added Test project - Tests for File Handler - App.config wiped to remove sensative data - you will need to provide your own information to run tests

---
+++ Media-CleanerTest/Controllers/DatabaseHandlerTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Media_Cleaner.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace Media_Cleaner.Controllers.Tests
+{
+    [TestClass()]
+    public class DatabaseHandlerTest
+    {
+        //[TestMethod()]
+        //public void QueryDatabaseMediaTest()
+        //{
+        //    string badConnectionString = ConfigurationManager.AppSettings["BadConnectionString"];
+        //    var data = DatabaseHandler.QueryDatabaseMedia(badConnectionString);
+
+            
+
+        //    Assert.Fail();
+        //}
+    }
+}
---
+++ Media-CleanerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Media-CleanerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Media-CleanerTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("684a3164-90b9-4d63-b603-e617ed869e4a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
